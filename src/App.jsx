80afe7455bd6a8151b0c8cdfbc393e6bef009529
Zlatan Bekric <zlaja@Zlatans-MacBook-Pro.local>
2023-09-15 18:03:09 +0200

    Setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React from 'react';
+import {Outlet, Route, Routes} from 'react-router';
 
+/**
+ *
+ * To be replaced with components
+ */
+const Header = () => <nav>Header</nav>;
+const Sidebar = () => <aside>Aside</aside>;
+
+const Home = () => <h1>Home</h1>;
+const Movies = () => <h1>Movies</h1>;
+const TvSeries = () => <h1>Tv Series</h1>;
+const Bookmarks = () => <h1>Bookmarks</h1>;
+const LogIn = () => <h1>Log in</h1>;
+const SignUp = () => <h1>Sign up</h1>;
+
+const NavigationLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Sidebar />
+    </>
+  );
+};
 function App() {
   return (
-   <></>
+    <Routes>
+      <Route path="/" Component={NavigationLayout}>
+        <Route index Component={Home} />
+        <Route path="/movies" Component={Movies} />
+        <Route path="/tv-series" Component={TvSeries} />
+        <Route path="/bookmarks" Component={Bookmarks} />
+      </Route>
+      <Route path="/sign-in" Component={LogIn} />
+      <Route path="/sign-up" Component={SignUp} />
+    </Routes>
   );
 }
 
