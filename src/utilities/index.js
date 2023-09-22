0669e4a47d44bf9ff6241add0468dec6a2f88af6
Darko22 <retcr2210@gmail.com>
2023-09-22 18:50:56 +0200

    isBookmarked

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -17,13 +17,11 @@ const getBookmarks = (id) => {
   }
 };
 
-// ne zna funkcija vec pitas localstorage
-
-// 1. da provjeri sa localStorage da li postoji ID
-// 2. ako postoji ID da ga skloni
-
-// Ako je localStorage null napravimo novi niz i u novi niz dodamo ID
-// nakon akcija dodavanja ili oduzimanja niza moramo da sacuvamo niz u localstorage
-//kada postoji LocalStorage prvo moramo da procitamo njegove vrijednosti, vrijednosti citamo sa json farse i onda ako taj ID postoji u tome nizu brisemo ga a ako ne postoji dodajemo ga
-
-// Ako smo vidjeli da niz nije prazan, provjerimo da li taj ID postoji u nizu a ako postoji izbaci ga iz niza a ako ne postoji ubaci ga
+export function isBookmarked(id) {
+  const bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  if (bookmarks && bookmarks.includes(id)) {
+    return true;
+  } else {
+    return false;
+  }
+}
